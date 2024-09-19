d0ec3ea3e344468d791d1efc69d338eeee56ab33
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-09-18 17:33:46 -0700

    Add update method to Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,24 @@
 class Board
+  attr_reader :display
+  def initialize 
+    @display = [
+      ['*', '*', '*', '*', '*', '*', '*'],
+      ['*', '*', '*', '*', '*', '*', '*'],
+      ['*', '*', '*', '*', '*', '*', '*'],
+      ['*', '*', '*', '*', '*', '*', '*'],
+      ['*', '*', '*', '*', '*', '*', '*'],
+      ['*', '*', '*', '*', '*', '*', '*']
+    ]
+  end
+
+  def update(column, piece)
+    index = display.length - 1
+    while index >= 0 do
+      if display[index][column] == '*'
+        @display[index][column] = piece
+        break
+      end
+      index -= 1
+    end
+  end
 end
