176c67512f855943df504483aadd28fb85cca595
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-09-24 14:51:49 -0700

    Add tests for initialize method in Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,17 @@
 require_relative '../lib/game'
 
 describe Game do
-  
+  describe 'initialize' do 
+    subject(:new_game) { described_class.new }
+    context 'when an instance of Game is initialized' do
+      it 'creates an array of two player objects' do
+        expect(new_game.players.length).to eq(2)
+        expect(new_game.players[0]).to be_an_instance_of(Player)
+        expect(new_game.players[1]).to be_an_instance_of(Player)
+      end
+      it 'creates a board object' do
+        expect(new_game.board).to be_an_instance_of(Board) 
+      end
+    end
+  end
 end
