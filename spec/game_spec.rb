67ab813d6872897cf95ad754e480e18ce756f588
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-09-25 17:15:23 -0700

    Add tests for play_game method in Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/game'
 describe Game do
   describe '#initialize' do 
     subject(:new_game) { described_class.new }
-    
+
     context 'when an instance of Game is initialized' do
       it 'creates an array of two player objects' do
         expect(new_game.players.length).to eq(2)
@@ -58,6 +58,87 @@ describe Game do
     context 'when there is no winner' do
       it "prints 'It's a tie!'" do
         expect { game.declare_winner }.to output("\nIt's a tie!\n").to_stdout
+      end
+    end
+  end
+
+  describe '#play_game' do
+    subject(:game) { described_class.new }
+
+    context 'when the game starts' do
+      before do
+        allow_any_instance_of(Kernel).to receive(:puts)
+        allow(game).to receive(:get_move).and_return(3, 4)
+        allow(game.board).to receive(:update)
+        allow(game.board).to receive(:four_in_a_row?).and_return(false, true)
+      end
+      it 'calls get_move and board.update for each player turn' do
+        expect(game).to receive(:get_move).twice
+        expect(game.board).to receive(:update).twice
+        game.play_game
+      end
+    end
+
+    context 'when player one wins' do
+      before do
+        allow_any_instance_of(Kernel).to receive(:puts)
+        allow(game).to receive(:get_move)
+        allow(game.board).to receive(:update)
+        allow(game.board).to receive(:four_in_a_row?).and_return(true)
+      end
+
+      it 'changes game_over to true' do
+        expect{ game.play_game }.to change{ game.game_over }.to(true)
+      end
+
+      it 'changes winner to player one' do
+        expect{ game.play_game }.to change{ game.winner }.to('Player One')
+      end
+
+      it 'declares player one as the winner' do
+        expect(game).to receive(:declare_winner).and_return('\nPlayer One is a winner!\n')
+        game.play_game
+      end
+    end
+
+    context 'when player two wins' do
+      before do
+        allow_any_instance_of(Kernel).to receive(:puts)
+        allow(game).to receive(:get_move)
+        allow(game.board).to receive(:update)
+        allow(game.board).to receive(:four_in_a_row?).and_return(false, true)
+      end
+
+      it 'changes game_over to true' do
+        expect { game.play_game }.to change { game.game_over }.to(true)
+      end
+
+      it 'changes winner to player two' do
+        expect { game.play_game }.to change { game.winner }.to('Player Two')
+      end
+
+      it 'declares player two as the winner' do
+        expect(game).to receive(:declare_winner).and_return("\nPlayer Two is the winner!\n")
+        game.play_game
+      end
+    end
+
+    context 'when the board gets filled and there is no winner' do
+      before do
+        allow_any_instance_of(Kernel).to receive(:puts)
+        allow(game).to receive(:get_move)
+        allow(game.board).to receive(:update)
+        allow(game.board).to receive(:four_in_a_row?).and_return(false)
+        allow(game.board).to receive(:full?).and_return(true)
+      end
+
+      it 'changes game_over to true' do 
+        expect { game.play_game }.to change { game.game_over}.to (true)
+      end
+
+      it 'declares a tie' do
+        expect(game).to receive(:declare_winner).and_return("\nIt's a tie!\n")
+        game.play_game
       end
     end
   end
